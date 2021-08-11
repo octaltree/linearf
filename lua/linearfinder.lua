ed38456c84de478a07a3802d1168c352ff849393
octaltree <octaltree@users.noreply.github.com>
2021-08-11 10:09:56 +0900

    tmp

--- lua/linearfinder.lua
+++ lua/linearfinder.lua
@@ -1,6 +1,9 @@
 local function start()
-    local bridge = require('linearfinder.bridge.luv')
-    print('start')
+    package.cpath = package.cpath .. ';' .. '/home/octaltree/workspace/linearfinder/lua/linearfinder/bridge/target/release/lib?.so';
+    local bridge = require('bridge')
+    --print(bridge.sum(2, 3))
+    --print('start')
+    bridge.spawn()
 end
 
 return {start = start}
--- lua/linearfinder/bridge/src/lib.rs
+++ lua/linearfinder/bridge/src/lib.rs
@@ -1,5 +1,41 @@
-pub mod extern_ffi {
-    pub fn hello_world() -> String { "Hello World!".to_owned() }
+use mlua::prelude::*;
+use std::process::Command;
+
+#[mlua::lua_module]
+fn bridge(lua: &Lua) -> LuaResult<LuaTable> {
+    let exports = lua.create_table()?;
+    // exports.set("sum", lua.create_function(sum)?)?;
+    // exports.set("used_memory", lua.create_function(used_memory)?)?;
+    // exports.set("check_userdata", lua.create_function(check_userdata)?)?;
+    exports.set("spawn", lua.create_function(spawn)?)?;
+    Ok(exports)
 }
 
-include!(concat!(env!("OUT_DIR"), "/ffi.rs"));
+fn sum(_: &Lua, (a, b): (i64, i64)) -> LuaResult<i64> { Ok(a + b) }
+
+fn used_memory(lua: &Lua, _: ()) -> LuaResult<usize> { Ok(lua.used_memory()) }
+
+fn check_userdata(_: &Lua, ud: MyUserData) -> LuaResult<i32> { Ok(ud.0) }
+
+fn spawn(_: &Lua, _: ()) -> LuaResult<()> {
+    let mut rt = tokio::runtime::Runtime::new()?;
+    rt.spawn(async {
+        Command::new("touch").arg("/tmp/foo").status().unwrap();
+    });
+    Ok(())
+}
+
+#[derive(Clone, Copy)]
+struct MyUserData(i32);
+
+impl LuaUserData for MyUserData {}
+
+//#[mlua::lua_module]
+// fn rust_module_second(lua: &Lua) -> LuaResult<LuaTable> {
+//    let exports = lua.create_table()?;
+//    exports.set("userdata", lua.create_userdata(MyUserData(123))?)?;
+//    Ok(exports)
+//}
+
+#[mlua::lua_module]
+fn rust_module_error(_: &Lua) -> LuaResult<LuaTable> { Err("custom module error".to_lua_err()) }
