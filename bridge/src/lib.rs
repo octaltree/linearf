6ad421f429de8880426087f66be6c9a95b8949f4
octaltree <octaltree@users.noreply.github.com>
2021-08-11 14:35:51 +0900

    tmp

--- bridge/src/lib.rs
+++ bridge/src/lib.rs
@@ -4,24 +4,15 @@ use std::process::Command;
 #[mlua::lua_module]
 fn bridge(lua: &Lua) -> LuaResult<LuaTable> {
     let exports = lua.create_table()?;
-    // exports.set("sum", lua.create_function(sum)?)?;
-    // exports.set("used_memory", lua.create_function(used_memory)?)?;
-    // exports.set("check_userdata", lua.create_function(check_userdata)?)?;
     exports.set("spawn", lua.create_function(spawn)?)?;
     Ok(exports)
 }
 
-fn sum(_: &Lua, (a, b): (i64, i64)) -> LuaResult<i64> { Ok(a + b) }
-
-fn used_memory(lua: &Lua, _: ()) -> LuaResult<usize> { Ok(lua.used_memory()) }
-
-fn check_userdata(_: &Lua, ud: MyUserData) -> LuaResult<i32> { Ok(ud.0) }
-
 fn spawn(_: &Lua, _: ()) -> LuaResult<()> {
     let mut rt = tokio::runtime::Runtime::new()?;
     Command::new("touch").arg("/tmp/bar").status().unwrap();
+    // TODO
     rt.spawn(async {
-        tokio::time::sleep(std::time::Duration::from_secs(10)).await;
         Command::new("touch").arg("/tmp/foo").status().unwrap();
     });
     Ok(())
@@ -31,13 +22,3 @@ fn spawn(_: &Lua, _: ()) -> LuaResult<()> {
 struct MyUserData(i32);
 
 impl LuaUserData for MyUserData {}
-
-//#[mlua::lua_module]
-// fn rust_module_second(lua: &Lua) -> LuaResult<LuaTable> {
-//    let exports = lua.create_table()?;
-//    exports.set("userdata", lua.create_userdata(MyUserData(123))?)?;
-//    Ok(exports)
-//}
-
-#[mlua::lua_module]
-fn rust_module_error(_: &Lua) -> LuaResult<LuaTable> { Err("custom module error".to_lua_err()) }
--- lua/linearf.lua
+++ lua/linearf.lua
@@ -5,9 +5,7 @@ local path = require('linearf.path')
 function M.start()
     package.cpath = package.cpath .. ';' .. path.bridge_signature()
     local bridge = require('bridge')
-    ----print(bridge.sum(2, 3))
-    ----print('start')
-    bridge.spawn()
+    print(bridge.spawn())
 end
 
 return M
